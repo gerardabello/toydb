416b592d5f89724b6fdd5fcd9aaf0d499a1dccbb
Gerard Abelló Serras <gerardabello@gmail.com>
2020-05-01 09:55:05 +0200

    add more benchmarks to kv_store

--- kv_store/benches/integration.rs
+++ kv_store/benches/integration.rs
@@ -1,22 +1,16 @@
 extern crate rand;
 
 use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
 
 use criterion::{black_box, criterion_group, criterion_main};
 
 use criterion::BenchmarkId;
 use criterion::Criterion;
-use criterion::Throughput;
 
 use kv_store;
 
-fn random_string() -> String {
-    thread_rng().sample_iter(&Alphanumeric).take(30).collect()
-}
-
 fn random_bytes() -> Vec<u8> {
-    random_string().into_bytes()
+    (0..20).map(|_| rand::random::<u8>()).collect()
 }
 
 fn add_value(c: &mut Criterion) {
@@ -37,27 +31,55 @@ fn add_value(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, add_value);
-criterion_main!(benches);
+fn get_value(c: &mut Criterion) {
+    let rng = rand::thread_rng();
 
-/*
-#![feature(test)]
+    let mut group = c.benchmark_group("get value in filled store");
+    for size in [1000, 10000, 100000].iter() {
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            let mut kv: kv_store::KVStore = Default::default();
 
-extern crate test;
-use test::Bencher;
+            // Get a random key from the added ones to search later
+            let search_key_index = rng.gen_range(0, size);
+            let mut search_key: Vec<u8> = vec![];
 
-
-
-#[bench]
-fn replacing_same_value(b: &mut Bencher) {
-b.iter(|| {
-let mut kv : kv_store::KVStore = Default::default();
-(0..BENCH_SIZE).map(|_| {
-kv.set("a", "mandarina");
-});
-
-kv.get("a")
-})
+            for i in 0..size {
+                let key = random_bytes();
+                let value = random_bytes();
+                if search_key_index == i {
+                    search_key = key.to_vec();
+                }
+                kv.set(key, value);
+            }
+            b.iter(|| {
+                kv.get(black_box(&search_key));
+            })
+        });
+    }
+    group.finish();
 }
 
-*/
+fn get_missing_value(c: &mut Criterion) {
+    let mut rng = rand::thread_rng();
+
+    let mut group = c.benchmark_group("get missing value in filled store");
+    for size in [1000, 10000, 100000].iter() {
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            let mut kv: kv_store::KVStore = Default::default();
+
+            for i in 0..size {
+                kv.set(random_bytes(), black_box(random_bytes()));
+            }
+
+            let search_key = random_bytes();
+            b.iter(|| {
+                kv.get(black_box(&search_key));
+            })
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, add_value, get_value, get_missing_value);
+
+criterion_main!(benches);
