b2043e6199e3cc60f2d09cd805a241c54efabdf7
Gerard Abelló Serras <gerardabello@gmail.com>
2020-05-14 17:52:34 +0200

    improve benchmarks

--- kv_store/benchmarks/src/main.rs
+++ kv_store/benchmarks/src/main.rs
@@ -9,6 +9,7 @@ use std::time::{Duration, Instant};
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
+const SAMPLES: u32 = 32;
 const TMP_DIR: &str = "./tmp-dir";
 
 fn black_box<T>(dummy: T) -> T {
@@ -31,13 +32,11 @@ fn shuffle_vec<T>(vec: &mut Vec<T>) {
     vec.shuffle(&mut thread_rng());
 }
 
-fn benchmark(benchmark_results: &mut kv_store::KVStore, name: &str, mut f: impl FnMut()) {
-    thread::sleep(Duration::from_millis(10));
-    let start = Instant::now();
-    f();
-    let duration = start.elapsed();
-    thread::sleep(Duration::from_millis(10));
-
+fn print_benchmark_result(
+    benchmark_results: &mut kv_store::KVStore,
+    name: &str,
+    duration: Duration,
+) {
     match benchmark_results.get(&serialize_string(name)) {
         Some(previous_duration_micros) => {
             let previous_duration = deserialize_duration(&previous_duration_micros);
@@ -54,32 +53,33 @@ fn benchmark(benchmark_results: &mut kv_store::KVStore, name: &str, mut f: impl 
     benchmark_results.set(serialize_string(name), serialize_duration(duration));
 }
 
-fn benchmark_kv_store(
-    mut benchmark_results: &mut kv_store::KVStore,
-    name: &str,
-    f: fn(&mut kv_store::KVStore) -> (),
-) {
-    let mut kv: kv_store::KVStore = kv_store::KVStore::new(TMP_DIR);
-    benchmark(&mut benchmark_results, name, || {
-        f(&mut kv);
-    });
-    std::mem::drop(kv);
-    fs::remove_dir_all(TMP_DIR).expect("Remove tmp folder");
+fn benchmark(mut f: impl FnMut()) -> Duration {
+    thread::sleep(Duration::from_millis(10));
+    let start = Instant::now();
+    f();
+    let duration = start.elapsed();
+    thread::sleep(Duration::from_millis(10));
+    duration
 }
 
-fn benchmark_kv_store_with_setup(
+fn benchmark_kv_store(
     mut benchmark_results: &mut kv_store::KVStore,
     name: &str,
     mut setup_f: impl FnMut(&mut kv_store::KVStore) -> (),
     mut f: impl FnMut(&mut kv_store::KVStore) -> (),
 ) {
-    let mut kv: kv_store::KVStore = kv_store::KVStore::new(TMP_DIR);
-    setup_f(&mut kv);
-    benchmark(&mut benchmark_results, name, || {
-        f(&mut kv);
-    });
-    std::mem::drop(kv);
-    fs::remove_dir_all(TMP_DIR).expect("Remove tmp folder");
+    let mut duration = Duration::new(0,0);
+    for _ in 0..SAMPLES {
+        let mut kv: kv_store::KVStore = kv_store::KVStore::new(TMP_DIR);
+        setup_f(&mut kv);
+        let d = benchmark(|| {
+            f(&mut kv);
+        });
+        duration += d;
+        std::mem::drop(kv);
+        fs::remove_dir_all(TMP_DIR).expect("Remove tmp folder");
+    }
+    print_benchmark_result(&mut benchmark_results, name, duration / SAMPLES);
 }
 
 fn serialize_duration(d: Duration) -> Vec<u8> {
@@ -110,6 +110,7 @@ fn main() {
     benchmark_kv_store(
         &mut benchmark_results,
         "Add 10_000 random entries to empty store",
+        |_| {},
         |kv| {
             for _ in 0..10_000 {
                 kv.set(random_bytes(), random_bytes());
@@ -120,6 +121,7 @@ fn main() {
     benchmark_kv_store(
         &mut benchmark_results,
         "Get 10_000 random entries in empty store",
+        |_| {},
         |kv| {
             for _ in 0..10_000 {
                 black_box(kv.get(&random_bytes()));
@@ -127,7 +129,7 @@ fn main() {
         },
     );
 
-    benchmark_kv_store_with_setup(
+    benchmark_kv_store(
         &mut benchmark_results,
         "Get 100 random entries in store with 100 random entries",
         |kv| {
@@ -142,7 +144,7 @@ fn main() {
         },
     );
 
-    benchmark_kv_store_with_setup(
+    benchmark_kv_store(
         &mut benchmark_results,
         "Get 100 random entries in store with 1000 random entries",
         |kv| {
@@ -157,7 +159,7 @@ fn main() {
         },
     );
 
-    benchmark_kv_store_with_setup(
+    benchmark_kv_store(
         &mut benchmark_results,
         "Get 100 random entries in store with 10_000 random entries",
         |kv| {
@@ -174,7 +176,7 @@ fn main() {
 
     {
         let entries = random_entries(100);
-        benchmark_kv_store_with_setup(
+        benchmark_kv_store(
             &mut benchmark_results,
             "Get 100 known entries in random order in store with 100 random entries",
             |kv| {
@@ -198,7 +200,7 @@ fn main() {
         let mut all_entries = random_entries(900);
         all_entries.extend(some_entries.iter().cloned());
 
-        benchmark_kv_store_with_setup(
+        benchmark_kv_store(
             &mut benchmark_results,
             "Get 100 known entries in random order in store with 1000 random entries",
             |kv| {
@@ -222,7 +224,7 @@ fn main() {
         let mut all_entries = random_entries(9900);
         all_entries.extend(some_entries.iter().cloned());
 
-        benchmark_kv_store_with_setup(
+        benchmark_kv_store(
             &mut benchmark_results,
             "Get 100 known entries in random order in store with 10_000 random entries",
             |kv| {
@@ -246,7 +248,7 @@ fn main() {
         let mut all_entries = random_entries(99900);
         all_entries.extend(some_entries.iter().cloned());
 
-        benchmark_kv_store_with_setup(
+        benchmark_kv_store(
             &mut benchmark_results,
             "Get 100 known entries in random order in store with 100_000 random entries",
             |kv| {
